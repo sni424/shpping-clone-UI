536d75bf7e5598273ffe1549fb3b1bf728f3dbd3
Your Name <you@example.com>
2022-03-19 03:50:46 +0900

    good

--- shpping-mall/src/components/Slider.jsx
+++ shpping-mall/src/components/Slider.jsx
@@ -1,5 +1,7 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons';
+import { useState } from 'react';
 import styled from 'styled-components';
+import { sliderItems } from '../data';
 import girl from '../img/womanmodel.jpg';
 
 const Container = styled.div`
@@ -7,6 +9,7 @@ const Container = styled.div`
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -25,10 +28,14 @@ const Arrow = styled.div`
   margin: auto;
   cursor: pointer;
   opacity: 0.5;
+  z-index: 2;
 `;
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
@@ -36,6 +43,7 @@ const Slide = styled.div`
   height: 100vh;
   display: flex;
   align-items: center;
+  background-color: #${(props) => props.bg};
 `;
 
 const ImgContainer = styled.div`
@@ -72,24 +80,35 @@ const Button = styled.button`
 `;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === 'left') {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick('left')}>
         <ArrowLeftOutlined />
       </Arrow>
-      <Wrapper>
-        <Slide>
-          <ImgContainer>
-            <Image src={girl} alt="girl" />
-          </ImgContainer>
-          <InfoContainer>
-            <Title>Summer SAL</Title>
-            <Desc>Chance For Take This Item</Desc>
-            <Button>Show Now</Button>
-          </InfoContainer>
-        </Slide>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg} key={item.id}>
+            <ImgContainer>
+              <Image src={process.env.PUBLIC_URL + `${item.img}`} alt="girl" />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>Show Now</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
       </Wrapper>
-      <Arrow direction="right">
+      <Arrow direction="right" onClick={() => handleClick('right')}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
---
+++ shpping-mall/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: '/img/womanmodel.jpg',
+    title: 'SUMMER SALE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: '/img/winter.jpg',
+    title: 'WINTER COLLECTION',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'fcf1ed',
+  },
+  {
+    id: 3,
+    img: '/img/best.jpg',
+    title: 'BEST SALE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'fbf0f4',
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: 'https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    title: 'SHIRT STYLE!',
+  },
+  {
+    id: 2,
+    img: 'https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    title: 'LOUNGEWEAR LOVE',
+  },
+  {
+    id: 3,
+    img: 'https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    title: 'LIGHT JACKETS',
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: '/img/jeans.jpg',
+  },
+  {
+    id: 2,
+    img: '/img/shoes.jpg',
+  },
+  {
+    id: 3,
+    img: '/img/blue-flower.jpg',
+  },
+  {
+    id: 4,
+    img: '/img/glasses.jpg',
+  },
+  {
+    id: 5,
+    img: '/img/buildings.jpg',
+  },
+  {
+    id: 6,
+    img: '/img/usa.jpg',
+  },
+  {
+    id: 7,
+    img: '/img/jeans.jpg',
+  },
+  {
+    id: 8,
+    img: '/img/bikini.jpg',
+  },
+];
